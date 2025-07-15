e236abed3a09c0ab800f616cf6adb60279c3e1cd
Nico Tena <nicoteja@hotmail.com>
2025-07-15 20:19:33 +0200

    refactor: Add Bootstrapper

---
+++ SpotifyDaily.Worker/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using SpotifyDaily.Worker.Models;
+using SpotifyDaily.Worker.Options;
+using SpotifyDaily.Worker.Services;
+using SpotifyDaily.Worker.Services.Contracts;
+
+namespace SpotifyDaily.Worker;
+
+public static class Bootstrapper
+{
+    public static IHostApplicationBuilder ConfigureSpotifyDaily(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IAppConfigService, AppConfigService>();
+        builder.Services.AddSingleton<ISpotifyClientService, SpotifyClientService>();
+        builder.Services.AddSingleton<IPlaylistService, PlaylistService>();
+
+
+        builder.Configuration.AddJsonFile("appconfig.json", optional: false, reloadOnChange: true);
+
+        builder.Services.Configure<AppConfig>(builder.Configuration.GetSection("AppConfig"));
+        builder.Services.Configure<SpotifyOptions>(builder.Configuration.GetSection("Spotify"));
+
+        return builder;
+    }
+}
--- SpotifyDaily.Worker/Program.cs
+++ SpotifyDaily.Worker/Program.cs
@@ -1,34 +1,21 @@
-using SpotifyDaily.Worker.Models;
-using SpotifyDaily.Worker.Options;
-using SpotifyDaily.Worker.Services;
-using SpotifyDaily.Worker.Services.Contracts;
+namespace SpotifyDaily.Worker;
 
-namespace SpotifyDaily.Worker
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddRouting();
-            builder.Services.AddControllers();
-            builder.Services.AddHostedService<Worker>();
+        builder.Services.AddRouting();
+        builder.Services.AddControllers();
+        builder.Services.AddHostedService<Worker>();
 
-            builder.Services.AddSingleton<IAppConfigService, AppConfigService>();
-            builder.Services.AddSingleton<ISpotifyClientService, SpotifyClientService>();
-            builder.Services.AddSingleton<IPlaylistService, PlaylistService>();
+        builder.ConfigureSpotifyDaily();
 
-            builder.Configuration.AddJsonFile("appconfig.json", optional: false, reloadOnChange: true);
-            builder.Services.Configure<AppConfig>(builder.Configuration.GetSection("AppConfig"));
+        var app = builder.Build();
 
-            builder.Services.Configure<SpotifyOptions>(builder.Configuration.GetSection("Spotify"));
+        app.MapControllers();
 
-            var app = builder.Build();
-
-            app.MapControllers();
-
-            app.Run();
-        }
+        app.Run();
     }
 }
