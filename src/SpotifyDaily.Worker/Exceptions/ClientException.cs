7d8ce964b21726a5869b1299115cf65f5a374671
Nico Tena <nicoteja@hotmail.com>
2025-07-18 22:04:52 +0200

    feat: #8: Implement editor rulesets (#11)

--- src/SpotifyDaily.Worker/Exceptions/ClientException.cs
+++ src/SpotifyDaily.Worker/Exceptions/ClientException.cs
@@ -1,16 +1,15 @@
-﻿namespace SpotifyDaily.Worker.Exceptions
+﻿namespace SpotifyDaily.Worker.Exceptions;
+
+public class ClientException : Exception
 {
-    public class ClientException : Exception
+    public ClientException() : base()
     {
-        public ClientException() : base()
-        {
-            
-        }
-        public ClientException(string message) : base(message)
-        {
-        }
-        public ClientException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+
+    }
+    public ClientException(string message) : base(message)
+    {
+    }
+    public ClientException(string message, Exception innerException) : base(message, innerException)
+    {
     }
 }
